a9c99962716b2509000ac2c0877d9f392d0886fe
Ismael Garcia <ismgar01@C02W2184HV2R.local>
2019-02-20 22:30:38 +0000

    Replace env. var names

--- libhu/hash_utils.h
+++ libhu/hash_utils.h
@@ -56,10 +56,8 @@ typedef signed long long       S64;
 
 #else
 
-#include <stdint.h>
-
-typedef unsigned int64_t       U64;
-typedef signed int64_t         S64;
+typedef unsigned __int64       U64;
+typedef signed __int64         S64;
 
 #define CUDA_CONST             static const
 #define CUDA_CONST_VAR         const
